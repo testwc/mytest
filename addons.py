565fa47bf232e71bf0a73b883e99a82c7480b248
testwc <wgcn310@qq.com>
2019-07-05 18:18:05 +0800

    Add files via upload

---
+++ addons.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# author:xiaochuan
+import mitmproxy.http
+from mitmproxy import ctx
+
+
+class Counter:
+    def __init__(self):
+        self.num = 0
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        self.num = self.num + 1
+        ctx.log.info("We've seen %d flows" % self.num)
+addons = [
+    Counter()
+]
